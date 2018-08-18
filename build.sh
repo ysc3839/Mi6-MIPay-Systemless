2520d34072fbc41c126aa78e30d6658cd40b3bbc
Richard Yu <yurichard3839@gmail.com>
2018-08-18 11:24:49 +0800

    Add error check.

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 . rom_urls.txt
 
-wget $MIUI_CN_DEV_ZIP
-unzip $(basename $MIUI_CN_DEV_ZIP) system.transfer.list system.new.dat
+wget $MIUI_CN_DEV_ZIP || exit 1
+unzip $(basename $MIUI_CN_DEV_ZIP) system.transfer.list system.new.dat || exit 1
 rm $(basename $MIUI_CN_DEV_ZIP)
-python3 sdat2img.py system.transfer.list system.new.dat
+python3 sdat2img.py system.transfer.list system.new.dat || exit 1
 rm system.transfer.list system.new.dat
-7z x -osystem system.img lib64/libuptsmaddonmi.so app/Mipay app/NextPay app/TSMClient app/UPTsmService
+7z x -osystem system.img lib64/libuptsmaddonmi.so app/Mipay app/NextPay app/TSMClient app/UPTsmService || exit 1
 rm system.img
 
 pushd system/app/TSMClient/lib/arm64
@@ -20,4 +20,4 @@ find -type d -exec chmod 0755 {} +
 find -type f -exec chmod 0644 {} +
 
 version=$(grep -Po "version=\K.*" module.prop)
-zip -r -x *.git* build.sh *.txt *.py *.zip -y -9 Mi6-MIPay-Systemless-$version.zip .
+zip -r -x *.git* build.sh *.txt *.py *.zip -y -9 Mi6-MIPay-Systemless-$version.zip . || exit 1
