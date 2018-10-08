2c548babda96a6e80885a90c06eddad569289e14
Richard Yu <yurichard3839@gmail.com>
2018-10-08 13:23:10 +0800

    Add MIUI Stable 10.0.1.0.

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 . rom_urls.txt
 
-wget $MIUI_CN_DEV_ZIP || exit 1
-unzip $(basename $MIUI_CN_DEV_ZIP) system.transfer.list system.new.dat || exit 1
-rm $(basename $MIUI_CN_DEV_ZIP)
+wget $MIUI_CN_STABLE_ZIP || exit 1
+unzip $(basename $MIUI_CN_STABLE_ZIP) system.transfer.list system.new.dat || exit 1
+rm $(basename $MIUI_CN_STABLE_ZIP)
 python3 sdat2img.py system.transfer.list system.new.dat || exit 1
 rm system.transfer.list system.new.dat
 7z x -osystem system.img lib64/libuptsmaddonmi.so app/Mipay app/NextPay app/TSMClient app/UPTsmService || exit 1
@@ -20,4 +20,4 @@ find -type d -exec chmod 0755 {} +
 find -type f -exec chmod 0644 {} +
 
 version=$(grep -Po "version=\K.*" module.prop)
-zip -r -x *.git* build.sh *.txt *.py *.zip *.yml -y -9 Mi6-MIPay-Systemless-$version.zip . || exit 1
+zip -r -x *.git* build.sh *.txt *.py *.zip *.yml -y -9 Mi6-MIPay-Systemless-MIUI-Stable-$version.zip . || exit 1
